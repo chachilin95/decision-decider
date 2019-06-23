4b39172f0fefedd3c886e2c2f71a36e9962e8f1c
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-23 14:16:22 -0500

    End of Video 10

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 console.log("App.js is running");
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1> 
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Alejandro Figueroa</h1>
+        <p>Age: 24</p>
+        <p>Location: Dallas, TX</p>
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
