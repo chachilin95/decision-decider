ececf8235c49601f5e3d77674a91021b9dc2d243
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-23 17:22:36 -0500

    End of Video 27

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -24,6 +37,21 @@ class Options extends React.Component {
         return (
             <div>
                 <p>Options component here</p>
+                <ol>
+                    <li><Option/></li>
+                    <li><Option/></li>
+                    <li><Option/></li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is an Option</p>
             </div>
         )
     }
@@ -39,12 +67,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-)
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
