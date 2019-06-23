e565a9781427d9517707e1d8e8be76e0f1225839
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-23 15:35:14 -0500

    End of Video 15 (2)

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,13 @@
+// var is function-level scoped
+var nameVar = "afigueroa"
+nameVar = "afig"
+console.log('nameVar', nameVar);
+
+// let is block-level scoped
+let nameLet = "Mike"
+nameLet = "Alfred"
+console.log("nameLet", nameLet);
+
+// const is block-level scoped
+const nameConst = "McKenna"
+console.log("nameConst", nameConst);
