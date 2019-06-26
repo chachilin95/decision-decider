e372d4870f329cc3f2c2f7adbe970b39e08f931a
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-06-25 21:41:01 -0500

    End of Video 45

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,6 +28,36 @@ var IndecisionApp = function (_React$Component) {
     }
 
     _createClass(IndecisionApp, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            try {
+                var json = localStorage.getItem('options');
+                var options = JSON.parse(json);
+
+                if (options) {
+                    this.setState(function () {
+                        return { options: options };
+                    });
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+
+            if (prevState.options.length !== this.state.options.length) {
+                var json = JSON.stringify(this.state.options);
+                localStorage.setItem('options', json);
+            }
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            console.log('componentWillUnmount');
+        }
+    }, {
         key: 'handleDeleteOptions',
         value: function handleDeleteOptions() {
             this.setState(function () {
@@ -141,6 +171,11 @@ var Options = function Options(props) {
             { onClick: props.handleDeleteOptions },
             'Remove All'
         ),
+        props.options.length === 0 && React.createElement(
+            'p',
+            null,
+            'Please add an option to get started!'
+        ),
         props.options.map(function (option) {
             return React.createElement(Option, {
                 key: option,
@@ -187,12 +222,16 @@ var AddOption = function (_React$Component2) {
         value: function handleOnSubmit(e) {
             e.preventDefault();
 
-            var option = e.target.elements.optionText.value.trim();
+            var option = e.target.elements.option.value.trim();
             var error = this.props.handleAddOption(option);
 
             this.setState(function () {
                 return { error: error };
             });
+
+            if (!error) {
+                e.target.elements.option.value = '';
+            }
         }
     }, {
         key: 'render',
@@ -208,7 +247,7 @@ var AddOption = function (_React$Component2) {
                 React.createElement(
                     'form',
                     { onSubmit: this.handleOnSubmit },
-                    React.createElement('input', { type: 'text', name: 'optionText' }),
+                    React.createElement('input', { type: 'text', name: 'option' }),
                     React.createElement(
                         'button',
                         null,
--- src/app.js
+++ src/app.js
@@ -13,6 +13,31 @@ class IndecisionApp extends React.Component {
         };
     }
 
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options }));
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        
+        if (prevState.options.length !== this.state.options.length) {            
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+
     handleDeleteOptions() {
         this.setState(() => ({ options: [] }));
     }
@@ -65,8 +90,7 @@ class IndecisionApp extends React.Component {
 
 IndecisionApp.defaultProps = {
     options: []
-}
-
+};
 
 const Header = (props) => {
     return (
@@ -97,6 +121,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option 
@@ -138,18 +163,22 @@ class AddOption extends React.Component {
     handleOnSubmit(e) {
         e.preventDefault();
 
-        const option = e.target.elements.optionText.value.trim();
+        const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
 
         this.setState(() => ({ error }));
-    }
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
+    }    
     
     render() {
         return (
             <div>
                 {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleOnSubmit}>
-                    <input type='text' name='optionText'/>
+                    <input type='text' name='option'/>
                     <button>Add Option</button>
                 </form>
             </div>
