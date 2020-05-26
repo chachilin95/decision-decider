c037e3ad942949064d3add68d32e3109f623a91e
Alejandro Figueroa <chachilin95@me.com>
2020-05-26 18:00:22 -0500

    Created state for options and selectedOption

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+    
+    const [options, updateOptions] = useState<string[]>([]);
+    const [selectedOption, updateSelectedOption] = useState(-1);
 
     return (
         <div>App goes here</div>
