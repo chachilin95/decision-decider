8e529121f57f130ebb1961ba5ccacee8ecaae3a9
Alejandro Figueroa <chachilin95@me.com>
2020-05-26 17:58:42 -0500

    Created App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+import IndecisionApp from './components/App';
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('root'));
